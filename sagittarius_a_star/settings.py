7b05df45c1010e29de8df27561dffa0c3bdd157c
NataliaCzeladka <natalia.czeladka@gmail.com>
2024-06-13 12:02:23 +0000

    Add allowed host and set debug to True

--- sagittarius_a_star/settings.py
+++ sagittarius_a_star/settings.py
@@ -26,12 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
-host = os.environ.get('HOST')
-if host:
-    ALLOWED_HOSTS.append(host)
+ALLOWED_HOSTS = ['8000-nataliaczel-sagittarius-oofu69qirxo.ws-eu114.gitpod.io']
 
 
 # Application definition
