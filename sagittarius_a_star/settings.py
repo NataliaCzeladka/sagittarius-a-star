e59facd9e11244eef33b3bdfbb2a402b290bfd0b
Natalia Czeladka <natalia.czeladka@gmail.com>
2024-06-24 13:53:47 +0000

    Edit bag context and add delivery logic

--- sagittarius_a_star/settings.py
+++ sagittarius_a_star/settings.py
@@ -161,6 +161,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
