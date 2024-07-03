60931bba0401f3cdfc26a6dfed98ac62eeed5d8c
Natalia Czeladka <natalia.czeladka@gmail.com>
2024-07-03 14:16:55 +0000

    Add Stripe elements to checkout app views

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P9WiVGdXdkbTqvHimWoBWdrwoxSq8Fg0z8TZqAPvUpC9eOS4uuyb8B9QY3uvQMw7YqsZOuAOBARzIYCwgccpT1200TgjknXm6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
