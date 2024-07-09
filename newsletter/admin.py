b183e65a16ce8396e9794590bf384fa4b35ab452
Natalia Czeladka <natalia.czeladka@gmail.com>
2024-07-09 20:24:07 +0000

    Add admin for newsletter app

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NewsletterSubscription
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(NewsletterSubscription, NewsletterAdmin)
