1bedb2a3d8ae69c8eb710de49c3ac1338db29cc8
Natalia Czeladka <natalia.czeladka@gmail.com>
2024-07-10 21:00:42 +0000

    Add admin for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ContactFormRequest
 
-# Register your models here.
+
+@admin.register(ContactFormRequest)
+class ContactFormRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'message', 'date_sent')
+    list_filter = ('date_sent')
+    search_fields = ('name', 'email', 'message')
