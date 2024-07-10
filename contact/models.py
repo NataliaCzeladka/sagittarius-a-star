80275b017024d3cd54f55997777b5842bd1851db
Natalia Czeladka <natalia.czeladka@gmail.com>
2024-07-10 20:40:40 +0000

    Add models to contact app and run migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ContactFormRequest(models.Model):
+    name = models.CharField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, blank=True)
+    message = models.TextField(null=False, blank=False)
+    date_sent = models.DateTimeField(default=timezone.now)
